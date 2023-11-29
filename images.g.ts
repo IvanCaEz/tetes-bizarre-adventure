39344ac8dec5f56b5c70e2c0a28e44b8483563b4
Joao <144930007+joaolopesdias12@users.noreply.github.com>
2023-11-29 17:04:33 +0100

    poopy enemy created

---
+++ images.g.ts
@@ -0,0 +1,131 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "poopy animated":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. f f f . . . f f f . . . . . . 
+f 1 1 b f . f 1 1 b f . . . . . 
+f f 1 b f f f f 1 b f . . . . . 
+f 1 1 b f e f 1 1 b f . . . . . 
+. f f f e e e f f f . . . . . . 
+. . f e e e e e e e f . . . . . 
+. f e e f f f e e e c f . . . . 
+. f e f 1 e 1 f e e c f . . . . 
+. f e e e e e e e e e c f . . . 
+. f e e e e e e e e e e c f . . 
+. f e e e e e e e e e e e c f . 
+. . f e e e e e e e e e e e c f 
+. . f e e e e e e e e e e c f . 
+. . . f e e e e e e e e f f . . 
+. . . . f f f f f f f f f . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. f f f . . . f f f . . . . . . 
+f 1 1 b f . f 1 1 b f . . . . . 
+f f 1 b f f f f 1 b f . . . . . 
+f 1 1 b f e f 1 1 b f . . . . . 
+. f f f e e e f f f . . . . . . 
+. . f e e e e e e c f . . . . . 
+. f e f f f e e e c f . . . . . 
+f e f 1 e 1 f e e c f . . . . . 
+f e e e e e e e e e c f . . . . 
+f e e e e e e e e e e c f . . . 
+f e e e e e e e e e e e c f . . 
+. f e e e e e e e e e e e c f . 
+. f e e e e e e e e e e c f . . 
+. . f e e e e e e e e f f . . . 
+. . . f f f f f f f f f . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . f f f . . . f f f . . . . . 
+. f 1 1 b f . f 1 1 b f . . . . 
+. f f 1 b f f f f 1 b f . . . . 
+. f 1 1 b f e f 1 1 b f . . . . 
+. . f f f e e e f f f . . . . . 
+. . . f e e e e e e e f . . . . 
+. . f e e f f f e e e c f . . . 
+. . f e f 1 e 1 f e e c f . . . 
+. f e e e e e e e e e c f . . . 
+. f e e e e e e e e e e c f . . 
+. f e e e e e e e e e e e c f . 
+. . f e e e e e e e e e e e c f 
+. . f e e e e e e e e e e c f . 
+. . . f e e e e e e e e f f . . 
+. . . . f f f f f f f f f . . . 
+`];
+            case "poopy animated 2":
+            case "anim2":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f . . . f f f . 
+. . . . . f b 1 1 f . f b 1 1 f 
+. . . . . f b 1 f f f f b 1 f f 
+. . . . . f b 1 1 f e f b 1 1 f 
+. . . . . . f f f e e e f f f . 
+. . . . . f e e e e e e e f . . 
+. . . . f c e e e f f f e e f . 
+. . . . f c e e f 1 e 1 f e f . 
+. . . f c e e e e e e e e e f . 
+. . f c e e e e e e e e e e f . 
+. f c e e e e e e e e e e e f . 
+f c e e e e e e e e e e e f . . 
+. f c e e e e e e e e e e f . . 
+. . f f e e e e e e e e f . . . 
+. . . f f f f f f f f f . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f . . . f f f . 
+. . . . . f b 1 1 f . f b 1 1 f 
+. . . . . f b 1 f f f f b 1 f f 
+. . . . . f b 1 1 f e f b 1 1 f 
+. . . . . . f f f e e e f f f . 
+. . . . . f c e e e e e e f . . 
+. . . . . f c e e e f f f e f . 
+. . . . . f c e e f 1 e 1 f e f 
+. . . . f c e e e e e e e e e f 
+. . . f c e e e e e e e e e e f 
+. . f c e e e e e e e e e e e f 
+. f c e e e e e e e e e e e f . 
+. . f c e e e e e e e e e e f . 
+. . . f f e e e e e e e e f . . 
+. . . . f f f f f f f f f . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . f f f . . . f f f . . 
+. . . . f b 1 1 f . f b 1 1 f . 
+. . . . f b 1 f f f f b 1 f f . 
+. . . . f b 1 1 f e f b 1 1 f . 
+. . . . . f f f e e e f f f . . 
+. . . . f e e e e e e e f . . . 
+. . . f c e e e f f f e e f . . 
+. . . f c e e f 1 e 1 f e f . . 
+. . . f c e e e e e e e e e f . 
+. . f c e e e e e e e e e e f . 
+. f c e e e e e e e e e e e f . 
+f c e e e e e e e e e e e f . . 
+. f c e e e e e e e e e e f . . 
+. . f f e e e e e e e e f . . . 
+. . . f f f f f f f f f . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
