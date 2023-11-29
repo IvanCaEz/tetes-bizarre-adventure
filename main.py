eb2e44f9123c8a76100267eed09939f5c64f2f66
Iván Martínez Cañero <125135728+IvanCaEz@users.noreply.github.com>
2023-11-29 18:34:08 +0100

    treasure collision fixed

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-# level.load(loadedLevel)
+@namespace
+class SpriteKind:
+    goal = SpriteKind.create()
+    # switch = SpriteKind.create()
+    treasure = SpriteKind.create()
 
 def on_hit_wall(sprite, location):
     if location.x == 136 and location.y == 8:
@@ -10,13 +14,29 @@ def on_hit_wall(sprite, location):
         scene.camera_shake(3, 500)
 scene.on_hit_wall(SpriteKind.player, on_hit_wall)
 
-def on_on_overlap_goal(SpriteKind, otherSprite):
+def create_enemies(level: number):
+    enemyList: List[Sprite] = []
+    if level == 1:
+        for index in range(5):
+            enemyList.append(sprites.create(assets.image("""
+                poopy left
+            """), SpriteKind.enemy))
+        for enemySprite in sprites.all_of_kind(SpriteKind.enemy):
+            enemySprite.set_position(Math.random_range(20, 300), Math.random_range(10, 220))
+
+def on_on_overlap_stairs(SpriteKind, otherSprite):
     if current_level == maxLevel:
         game.over(True)
     else:
         stair_sprite.destroy()
         current_level + 1
-sprites.on_overlap(SpriteKind.player, SpriteKind.goal, on_on_overlap_goal)
+sprites.on_overlap(SpriteKind.player, SpriteKind.goal, on_on_overlap_stairs)
+
+def on_on_overlap_treasure(SpriteKind, otherSprite):
+    info.set_score(100)
+    treasure_sprite.set_image(sprites.dungeon.chest_open)
+sprites.on_overlap(SpriteKind.player, SpriteKind.treasure, on_on_overlap_treasure)
+
 
 def create_level_one():
     global treasure_sprite, stair_sprite
@@ -32,45 +52,33 @@ def create_level_one():
 treasure_sprite: Sprite = None
 stair_sprite: Sprite = None
 maxLevel = 0
-def create_enemies(level: int):
-    enemyList = []
-    if (level == 1):
-        for i in range(5):
-            enemyList.push(sprites.create(assets.image("""
-                poopy left
-            """), SpriteKind.Enemy))
-        for enemySprite in sprites.allOfKind(SpriteKind.Enemy):
-            enemySprite.setPosition(Math.randomRange(20, 300), Math.randomRange(10, 220));
-switch_pulled = False
-switch_sprite = None
 current_level = 0
 player_sprite = None
+switch_sprite = None
+switch_pulled = False
+
 
 def on_update_interval():
-    for enemy in sprites.allOfKind(SpriteKind.Enemy):
-           # follow the player
-            if (enemy.x < Player.player_sprite.x):
-                enemy.vx = 15
-            else:
-                enemy.vx = -15
-            if (enemy.y < Player.player_sprite.y):
-                enemy.vy = 15
-            else:
-                enemy.vy = -15
-            
+    for enemy in sprites.all_of_kind(SpriteKind.enemy):
+        # follow the player
+        if enemy.x < Player.player_sprite.x:
+            enemy.vx = 15
+        else:
+            enemy.vx = -15
+        if enemy.y < Player.player_sprite.y:
+            enemy.vy = 15
+        else:
+            enemy.vy = -15
 game.on_update_interval(1000, on_update_interval)
 
-@namespace
-class SpriteKind:
-    goal = SpriteKind.create()
-    # switch = SpriteKind.create()
-    treasure = SpriteKind.create()
-maxLevel = 3
+current_level = 1
+maxLevel = 4
 create_level_one()
+
 @namespace
 class Player:
     player_sprite = sprites.create(sprites.duck.duck1, SpriteKind.player)
     info.set_life(3)
     scene.camera_follow_sprite(Player.player_sprite)
     controller.move_sprite(Player.player_sprite, 100, 100)
-    print(switch_pulled)
+
--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
-//  level.load(loadedLevel)
+namespace SpriteKind {
+    export const goal = SpriteKind.create()
+    //  switch = SpriteKind.create()
+    export const treasure = SpriteKind.create()
+}
+
 scene.onHitWall(SpriteKind.Player, function on_hit_wall(sprite: Sprite, location: tiles.Location) {
     if (location.x == 136 && location.y == 8) {
         info.setScore(200)
@@ -10,7 +15,22 @@ scene.onHitWall(SpriteKind.Player, function on_hit_wall(sprite: Sprite, location
     }
     
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.goal, function on_on_overlap_goal(SpriteKind: Sprite, otherSprite: Sprite) {
+function create_enemies(level: number) {
+    let enemyList : Sprite[] = []
+    if (level == 1) {
+        for (let index = 0; index < 5; index++) {
+            enemyList.push(sprites.create(assets.image`
+                poopy left
+            `, SpriteKind.Enemy))
+        }
+        for (let enemySprite of sprites.allOfKind(SpriteKind.Enemy)) {
+            enemySprite.setPosition(Math.randomRange(20, 300), Math.randomRange(10, 220))
+        }
+    }
+    
+}
+
+sprites.onOverlap(SpriteKind.Player, SpriteKind.goal, function on_on_overlap_stairs(SpriteKind: Sprite, otherSprite: Sprite) {
     if (current_level == maxLevel) {
         game.over(true)
     } else {
@@ -18,6 +38,10 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.goal, function on_on_overlap_goa
         current_level + 1
     }
     
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.treasure, function on_on_overlap_treasure(SpriteKind: Sprite, otherSprite: Sprite) {
+    info.setScore(100)
+    treasure_sprite.setImage(sprites.dungeon.chestOpen)
 })
 function create_level_one() {
     
@@ -34,25 +58,10 @@ function create_level_one() {
 let treasure_sprite : Sprite = null
 let stair_sprite : Sprite = null
 let maxLevel = 0
-function create_enemies(level: number) {
-    let enemyList = []
-    if (level == 1) {
-        for (let i = 0; i < 5; i++) {
-            enemyList.push(sprites.create(assets.image`
-                poopy left
-            `, SpriteKind.Enemy))
-        }
-        for (let enemySprite of sprites.allOfKind(SpriteKind.Enemy)) {
-            enemySprite.setPosition(Math.randomRange(20, 300), Math.randomRange(10, 220))
-        }
-    }
-    
-}
-
-let switch_pulled = false
-let switch_sprite = null
 let current_level = 0
 let player_sprite = null
+let switch_sprite = null
+let switch_pulled = false
 game.onUpdateInterval(1000, function on_update_interval() {
     for (let enemy of sprites.allOfKind(SpriteKind.Enemy)) {
         //  follow the player
@@ -70,19 +79,13 @@ game.onUpdateInterval(1000, function on_update_interval() {
         
     }
 })
-namespace SpriteKind {
-    export const goal = SpriteKind.create()
-    //  switch = SpriteKind.create()
-    export const treasure = SpriteKind.create()
-}
-
-maxLevel = 3
+current_level = 1
+maxLevel = 4
 create_level_one()
 namespace Player {
     export const player_sprite = sprites.create(sprites.duck.duck1, SpriteKind.Player)
     info.setLife(3)
     scene.cameraFollowSprite(Player.player_sprite)
     controller.moveSprite(Player.player_sprite, 100, 100)
-    console.log(switch_pulled)
 }
 
