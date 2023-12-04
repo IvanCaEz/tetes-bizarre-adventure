8857cb27eebf122355563677220504012e8de4fc
Joao <144930007+joaolopesdias12@users.noreply.github.com>
2023-12-04 18:24:28 +0100

    avatar added

--- images.g.ts
+++ images.g.ts
@@ -1,106 +1,16 @@
-// Código generado automáticamente. No editar.
+// Auto-generated code. Do not edit.
 namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image2":
-            case "poopy right":return img`
-. . . . . . . . . . . . . . . . 
-. . . . . f f f . . . f f f . . 
-. . . . f b 1 1 f . f b 1 1 f . 
-. . . . f b 1 f f f f b 1 f f . 
-. . . . f b 1 1 f e f b 1 1 f . 
-. . . . . f f f e e e f f f . . 
-. . . . f e e e e e e e f . . . 
-. . . f c e e e f f f e e f . . 
-. . . f c e e f 1 e 1 f e f . . 
-. . . f c e e e e e e e e e f . 
-. . f c e e e e e e e e e e f . 
-. f c e e e e e e e e e e e f . 
-f c e e e e e e e e e e e f . . 
-. f c e e e e e e e e e e f . . 
-. . f f e e e e e e e e f . . . 
-. . . f f f f f f f f f . . . . 
-`;
-            case "image1":
-            case "poopy left":return img`
-. . . . . . . . . . . . . . . . 
-. . f f f . . . f f f . . . . . 
-. f 1 1 b f . f 1 1 b f . . . . 
-. f f 1 b f f f f 1 b f . . . . 
-. f 1 1 b f e f 1 1 b f . . . . 
-. . f f f e e e f f f . . . . . 
-. . . f e e e e e e e f . . . . 
-. . f e e f f f e e e c f . . . 
-. . f e f 1 e 1 f e e c f . . . 
-. f e e e e e e e e e c f . . . 
-. f e e e e e e e e e e c f . . 
-. f e e e e e e e e e e e c f . 
-. . f e e e e e e e e e e e c f 
-. . f e e e e e e e e e e c f . 
-. . . f e e e e e e e e f f . . 
-. . . . f f f f f f f f f . . . 
-`;
+
         }
         return null;
     })
 
     helpers._registerFactory("animation", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "poopy animated right":
-            case "anim2":return [img`
-. . . . . . . . . . . . . . . . 
-. . . . . . f f f . . . f f f . 
-. . . . . f b 1 1 f . f b 1 1 f 
-. . . . . f b 1 f f f f b 1 f f 
-. . . . . f b 1 1 f e f b 1 1 f 
-. . . . . . f f f e e e f f f . 
-. . . . . f e e e e e e e f . . 
-. . . . f c e e e f f f e e f . 
-. . . . f c e e f 1 e 1 f e f . 
-. . . f c e e e e e e e e e f . 
-. . f c e e e e e e e e e e f . 
-. f c e e e e e e e e e e e f . 
-f c e e e e e e e e e e e f . . 
-. f c e e e e e e e e e e f . . 
-. . f f e e e e e e e e f . . . 
-. . . f f f f f f f f f . . . . 
-`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . f f f . . . f f f . 
-. . . . . f b 1 1 f . f b 1 1 f 
-. . . . . f b 1 f f f f b 1 f f 
-. . . . . f b 1 1 f e f b 1 1 f 
-. . . . . . f f f e e e f f f . 
-. . . . . f c e e e e e e f . . 
-. . . . . f c e e e f f f e f . 
-. . . . . f c e e f 1 e 1 f e f 
-. . . . f c e e e e e e e e e f 
-. . . f c e e e e e e e e e e f 
-. . f c e e e e e e e e e e e f 
-. f c e e e e e e e e e e e f . 
-. . f c e e e e e e e e e e f . 
-. . . f f e e e e e e e e f . . 
-. . . . f f f f f f f f f . . . 
-`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . f f f . . . f f f . . 
-. . . . f b 1 1 f . f b 1 1 f . 
-. . . . f b 1 f f f f b 1 f f . 
-. . . . f b 1 1 f e f b 1 1 f . 
-. . . . . f f f e e e f f f . . 
-. . . . f e e e e e e e f . . . 
-. . . f c e e e f f f e e f . . 
-. . . f c e e f 1 e 1 f e f . . 
-. . . f c e e e e e e e e e f . 
-. . f c e e e e e e e e e e f . 
-. f c e e e e e e e e e e e f . 
-f c e e e e e e e e e e e f . . 
-. f c e e e e e e e e e e f . . 
-. . f f e e e e e e e e f . . . 
-. . . f f f f f f f f f . . . . 
-`];
-            case "poopy animated left":
+            case "poopy animated":
             case "anim1":return [img`
 . . . . . . . . . . . . . . . . 
 . f f f . . . f f f . . . . . . 
@@ -153,20 +63,139 @@ f e e e e e e e e e e e c f . .
 . . . f e e e e e e e e f f . . 
 . . . . f f f f f f f f f . . . 
 `];
+            case "poopy animated 2":
+            case "anim2":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f . . . f f f . 
+. . . . . f b 1 1 f . f b 1 1 f 
+. . . . . f b 1 f f f f b 1 f f 
+. . . . . f b 1 1 f e f b 1 1 f 
+. . . . . . f f f e e e f f f . 
+. . . . . f e e e e e e e f . . 
+. . . . f c e e e f f f e e f . 
+. . . . f c e e f 1 e 1 f e f . 
+. . . f c e e e e e e e e e f . 
+. . f c e e e e e e e e e e f . 
+. f c e e e e e e e e e e e f . 
+f c e e e e e e e e e e e f . . 
+. f c e e e e e e e e e e f . . 
+. . f f e e e e e e e e f . . . 
+. . . f f f f f f f f f . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f . . . f f f . 
+. . . . . f b 1 1 f . f b 1 1 f 
+. . . . . f b 1 f f f f b 1 f f 
+. . . . . f b 1 1 f e f b 1 1 f 
+. . . . . . f f f e e e f f f . 
+. . . . . f c e e e e e e f . . 
+. . . . . f c e e e f f f e f . 
+. . . . . f c e e f 1 e 1 f e f 
+. . . . f c e e e e e e e e e f 
+. . . f c e e e e e e e e e e f 
+. . f c e e e e e e e e e e e f 
+. f c e e e e e e e e e e e f . 
+. . f c e e e e e e e e e e f . 
+. . . f f e e e e e e e e f . . 
+. . . . f f f f f f f f f . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . f f f . . . f f f . . 
+. . . . f b 1 1 f . f b 1 1 f . 
+. . . . f b 1 f f f f b 1 f f . 
+. . . . f b 1 1 f e f b 1 1 f . 
+. . . . . f f f e e e f f f . . 
+. . . . f e e e e e e e f . . . 
+. . . f c e e e f f f e e f . . 
+. . . f c e e f 1 e 1 f e f . . 
+. . . f c e e e e e e e e e f . 
+. . f c e e e e e e e e e e f . 
+. f c e e e e e e e e e e e f . 
+f c e e e e e e e e e e e f . . 
+. f c e e e e e e e e e e f . . 
+. . f f e e e e e e e e f . . . 
+. . . f f f f f f f f f . . . . 
+`];
+            case "myAnim":
+            case "anim3":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . f d d d d d c f . . . . 
+. . . f d d d d d d b c f . . . 
+. . f d d d d d d d d b c f . . 
+. . f d d 1 f d d f 1 b c f . . 
+. . f d d 1 f d d f 1 b c f . . 
+. . f d d d d d d d d b c f . . 
+. . f d d d d f f d d b c f . . 
+. . . f d d d d d d b c f . . . 
+. . 7 7 f 7 1 7 1 7 1 f 7 7 . . 
+. . b d f 7 1 7 1 7 1 f d b . . 
+. . d b f 6 6 6 6 6 6 f b d . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . f d d d d b c f . . . . 
+. . . f d d d d d d b c f . . . 
+. . f d d d d d d d d b c f . . 
+. . f d d d d d d d d b c f . . 
+. . f d d 1 f d d f 1 b c f . . 
+. . f d d 1 f d d f 1 b c f . . 
+. . f d d d d d d d d b c f . . 
+. . . f d d d d d d b c f f . . 
+. . f 7 f 7 1 7 1 f d b 7 f . . 
+. . 7 d f 7 1 7 1 f b d f . . . 
+. . . . f 6 6 6 6 f f f . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . f f f . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . f d d d d d c f . . . . 
+. . . f d d d d d d b c f . . . 
+. . f d d d d d d d d b c f . . 
+. . f d d 1 f d d f 1 b c f . . 
+. . f d d 1 f d d f 1 b c f . . 
+. . f d d d d d d d d b c f . . 
+. . f d d d d f f d d b c f . . 
+. . . f d d d d d d b c f . . . 
+. . 7 7 f 7 1 7 1 7 1 f 7 7 . . 
+. . b d f 7 1 7 1 7 1 f d b . . 
+. . d b f 6 6 6 6 6 6 f b d . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . f d d d d b c f . . . . 
+. . . f d d d d d d b c f . . . 
+. . f d d d d d d d d b c f . . 
+. . f d d d d d d d d b c f . . 
+. . f d d 1 f d d f 1 b c f . . 
+. . f d d 1 f d d f 1 b c f . . 
+. . f d d d d d d d d b c f . . 
+. . f f d d d d d d b c f . . . 
+. . f 7 b d f 1 7 1 7 f 7 f . . 
+. . . f d b f 1 7 1 7 f d 7 . . 
+. . . . f f f 6 6 6 6 f . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . . . . . . f f f . . . . 
+`];
         }
         return null;
     })
 
     helpers._registerFactory("song", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "mySong":return hex`0078000408010400001c00010a006400f401640000040000000000000000000000000005000004060000000400012905001c000f0a006400f4010a00000400000000000000000000000000000000021b0004000800021b1e08000c0001220c001000021d2418001c0002192007001c00020a006400f401640000040000000000000000000000000000000003060004000800012509010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c8000d0008000900010710001100020108`;
-            case "song1":
-            case "level one song short":return hex`0078000408010207001c00020a006400f4016400000400000000000000000000000000000000031b00000004000222250c00100002252914001800021e2418001c00011d09010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c8002c00000001000109040005000203060800090001091000110001051400150001021800190001061c001d00020009`;
-            case "song2":
-            case "level one bso":return hex`0078000408020305001c000f0a006400f4010a0000040000000000000000000000000000000002240000000400011d0c001000011d18001c00012724002800012030003400011d3c004000012007001c00020a006400f4016400000400000000000000000000000000000000034100000004000222250c00100002252914001800021e2418001c00011d1c002000012024002800012728002c0001243000340002292c38003c00021e253c004000011d09010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c8005b000000010001090400050002030608000900020009100011000200051400150001021800190001061c001d000200092000210002040728002900030004082c002d0001093000310001053400350001003800390001023c003d000109`;
+
         }
         return null;
     })
 
 }
-// Código generado automáticamente. No editar.
+// Auto-generated code. Do not edit.
